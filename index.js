530f0c1cd3d46bb166a06e801ad1445f4004ec2d
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-25 19:00:31 +0530

    Commit id

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express()
 const pinRoute = require("./routes/mapPins")
+const cors = require('cors');
+
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 dotenv.config()
 
