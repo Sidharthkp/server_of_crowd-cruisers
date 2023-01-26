b5ba9aad5aca9f916ffc5a97e16a84170e7190fe
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-26 17:39:12 +0530

    Socket io connected

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const dotenv = require("dotenv");
 const app = express()
 const pinRoute = require("./routes/mapPins")
 const cors = require('cors');
+const { Server } = require('socket.io');
+http = require('http');
 
-const corsOptions ={
-    origin:'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+const server = http.createServer(app); // Add this
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 
@@ -16,14 +20,28 @@ dotenv.config()
 
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Database is connected");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 });
 
 app.use("/api/pins", pinRoute)
 
-app.listen(3000, () => {
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+    },
+});
+
+// Listen for when the client connects via socket.io-client
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
+
+    // We can write our socket event listeners in here...
+});
+
+server.listen(3000, () => {
     console.log("Server has started at 3000");
 })
