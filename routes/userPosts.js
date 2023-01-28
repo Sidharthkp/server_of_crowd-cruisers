b36147380e823b1c67ee19dd18a0c5fa86a0007f
Sidharthkp <sidharthkannan20@gmail.com>
2023-01-28 16:06:56 +0530

    Commit home page worked

--- routes/userPosts.js
+++ routes/userPosts.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const fs = require("fs");
+const url = require("url");
+
+const path = require("path");
 
 const multer = require("multer");
 
@@ -32,10 +36,61 @@ const upload = multer({
     fileFilter: fileFilter,
 });
 
+router.use("/image", (req, res) => {
+    // Parsing the URL
+    var request = url.parse(req.url, true);
+
+    // console.log(request.query);
+
+    // Extracting the path of file
+    var action = request.pathname;
+    // console.log(action);
+    
+    var filePath = path.join(__dirname,
+        action).split("%20").join(" ");
+        
+        // console.log(filePath);
+    fs.exists(filePath, function (exists) {
+        if (!exists) {
+            res.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            res.end("404 Not Found");
+            return;
+        }
+
+        // Extracting file extension
+        var ext = path.extname(action);
+
+        // Setting default Content-Type
+        var contentType = "text/plain";
+
+        // Checking if the extension of
+        // image is '.png'
+        if (ext === ".png") {
+            contentType = "image/png";
+        }
+
+        // Setting the headers
+        res.writeHead(200, {
+            "Content-Type": contentType
+        });
+
+        // Reading the file
+        fs.readFile(""+request.query.q,
+            function (err, content) {
+                // Serving the image
+                res.end(content);
+            });
+    });
+})
+
+// router.use("/", require("express").static(path.join(__dirname, "./public/Images")));
+
 router.get("/", (req, res) => {
+    console.log("Reached here");
     Posts.find()
-        .select("name postImage -_id ")
-        .then((pro) => res.json(pro))
+        .then((posts) => res.json(posts))
         .catch((err) => res.json({ error: "could not get posts", err }));
 });
 
